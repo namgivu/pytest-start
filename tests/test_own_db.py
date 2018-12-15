af4fe8b9ddfdeeba15691f2929fb79be61eb8922
Nam G VU <namgivu@gmail.com>
2018-12-16 03:04:00 +0700

    code 0th

---
+++ tests/test_own_db.py
@@ -0,0 +1,13 @@
+import unittest
+
+def setUpModule():    pass # nothing here for now
+def tearDownModule(): pass # nothing here for now
+
+
+class TestOwnDb(unittest.TestCase):
+
+    def setUp(self):    pass # nothing here for now
+    def tearDown(self): pass # nothing here for now
+
+
+    def test_own_db(self): pass

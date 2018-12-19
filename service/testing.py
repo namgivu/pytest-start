fe972054ff104b4c1a166adabf835130ed8e7633
namgivu <namgivu@gmail.com>
2018-12-19 20:57:00 +0700

    todo

---
+++ service/testing.py
@@ -0,0 +1,10 @@
+import service.postgres
+
+def set_up_db(db_name):
+    #TODO create database :db_name
+    #TODO make service.postgres.DbSvc#get_session() use this :db_name
+    pass
+
+def tear_down_db(db_name):
+    #TODO drop database :db_name
+    pass
--- tests/test_own_db.py
+++ tests/test_own_db.py
@@ -1,4 +1,5 @@
 import unittest
+import service.testing
 
 def setUpModule():    pass # nothing here for now
 def tearDownModule(): pass # nothing here for now
@@ -6,8 +7,16 @@ def tearDownModule(): pass # nothing here for now
 
 class TestOwnDb(unittest.TestCase):
 
-    def setUp(self):    pass # nothing here for now
-    def tearDown(self): pass # nothing here for now
+    def tid(self):
+        return unittest.TestCase.id(self)
 
 
-    def test_own_db(self): pass
+    def setUp(self):
+        service.testing.set_up_db(self.tid())
+
+    def tearDown(self):
+        service.testing.tear_down_db(self.tid())
+
+
+    def test_own_db(self):
+        abb = 122

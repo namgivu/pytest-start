c79105ffb48c5543cb809d78e84a0163f6a56311
namgivu <nam.gi.vu@gigacover.com>
2019-11-06 12:19:02 +0700

    spacing

--- tests/test_parallel_run.py
+++ tests/test_parallel_run.py
@@ -1,14 +1,14 @@
 import time
 import unittest
 
-def setUpModule():    pass # nothing here for now
-def tearDownModule(): pass # nothing here for now
+def setUpModule():    pass  # nothing here for now
+def tearDownModule(): pass  # nothing here for now
 
 
 class TestParallelRun(unittest.TestCase):
 
-    def setUp(self):    pass # nothing here for now
-    def tearDown(self): pass # nothing here for now
+    def setUp(self):    pass  # nothing here for now
+    def tearDown(self): pass  # nothing here for now
 
 
     def test_parallel_run1(self):

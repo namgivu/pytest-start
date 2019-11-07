4119fd2bae8f8d80b305a9a986f77b683e019f55
namgivu <nam.gi.vu@gigacover.com>
2019-11-08 06:48:36 +0700

    review

--- tests/test_dotenv.py
+++ tests/test_dotenv.py
@@ -6,7 +6,7 @@ def setUpModule():    pass   # nothing here for now
 def tearDownModule(): pass   # nothing here for now
 
 
-class TestParallelRun(unittest.TestCase):
+class Test(unittest.TestCase):
 
     def setUp(self):    pass   # nothing here for now
     def tearDown(self): pass   # nothing here for now

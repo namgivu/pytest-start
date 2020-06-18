1202e0d0aa9b8d47663d07bfbe82afb7f9f8b728
namgivu <namgivu@gmail.com>
2020-06-18 15:58:15 +0700

    code under src.util/

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -1,14 +1,14 @@
 class Test:
 
     def test_when_no_mock(self):
-        from demo_mock import longlong_time_to_run
+        from src.util.demo_mock import longlong_time_to_run
         v = longlong_time_to_run()  # will take VERY LONG to finish
         assert v==122
 
 
     def test_with_mock(self):
         from mockito import when
-        import demo_mock
+        from src.util import demo_mock
 
         # mock longlong_time_to_run() to return immediately 333
         # ref. mockito https://pypi.org/project/mockito/

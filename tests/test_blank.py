1ee09440f18f84d71278294427f5e60e81bb3f71
namgivu <namgivu@gmail.com>
2020-06-18 15:53:38 +0700

    simpler blank

--- tests/test_blank.py
+++ tests/test_blank.py
@@ -1,14 +1,4 @@
-import unittest
-
-def setUpModule():    pass   # nothing here for now
-def tearDownModule(): pass   # nothing here for now
-
-
-class TestParallelRun(unittest.TestCase):
-
-    def setUp(self):    pass   # nothing here for now
-    def tearDown(self): pass   # nothing here for now
-
+class TestParallelRun:
 
     def test(self):
         pass
